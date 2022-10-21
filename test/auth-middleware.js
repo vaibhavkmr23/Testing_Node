be01584e781a53d6e09173feb7fc73334b507797
vaibhav <vaibhavkumarjha46@gmail.com>
2022-10-21 11:00:24 +0530

    Should not test functionality of 3rd party package like verify method of jwt

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -14,9 +14,28 @@ describe('Auth middleware', function () {
     it('should throw an error if the authorization header is only string', function () {
         const req = {
             get: function (headerName) {
-                return null
+                return 'xysa'
             }
         }
         expect(authMiddleware.bind(this, req, {}, () => { })).to.throw();
     })
+
+    it('should throw an error if token cannot be verified', function () {
+        const req = {
+            get: function (headerName) {
+                return 'Bearer xyz'
+            }
+        };
+        expect(authMiddleware.bind(this, req, {}, () => { })).to.throw()
+    });
+
+    it('should should yield a userId after decoding the token', function () {
+        const req = {
+            get: function (headerName) {
+                return 'Bearer kslslslskk'
+            }
+        };
+        authMiddleware(req, {}, () => { });
+        expect(req).to.have.property('userId');
+    });
 })
