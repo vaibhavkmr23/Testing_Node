79bf274b4594bbbfc0836520aa0b7bba8d642b8f
vaibhav <vaibhavkumarjha46@gmail.com>
2022-10-21 12:29:26 +0530

    Using Stubs

--- test/auth-middleware.js
+++ test/auth-middleware.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
 const authMiddleware = require('../middleware/is-auth');
+const jwt = require('jsonwebtoken');
+const sinon = require('sinon');
 
 describe('Auth middleware', function () {
     it('should throw an error if no authorization header is present', function () {
@@ -20,6 +22,21 @@ describe('Auth middleware', function () {
         expect(authMiddleware.bind(this, req, {}, () => { })).to.throw();
     })
 
+    it('should should yield a userId after decoding the token', function () {
+        const req = {
+            get: function (headerName) {
+                return 'Bearer kslslslskk'
+            }
+        };
+        sinon.stub(jwt, 'verify');
+        jwt.verify.returns({ userId: 'abc' })
+        authMiddleware(req, {}, () => { });
+        expect(req).to.have.property('userId');
+        expect(req).to.have.property('userId', 'abc');
+        expect(jwt.verify.called).to.be.true;
+        jwt.verify.restore();
+    });
+
     it('should throw an error if token cannot be verified', function () {
         const req = {
             get: function (headerName) {
@@ -29,13 +46,5 @@ describe('Auth middleware', function () {
         expect(authMiddleware.bind(this, req, {}, () => { })).to.throw()
     });
 
-    it('should should yield a userId after decoding the token', function () {
-        const req = {
-            get: function (headerName) {
-                return 'Bearer kslslslskk'
-            }
-        };
-        authMiddleware(req, {}, () => { });
-        expect(req).to.have.property('userId');
-    });
+
 })
